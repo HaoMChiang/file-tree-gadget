910513f93f0cc8937bf1e93ec5fef97d269f9c22
Hao Chiang <haomingchiang@gmail.com>
2021-03-24 15:31:55 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+function App() {
+  return (
+    <div>
+      <h2>Hello</h2>
+      <Folder />
+    </div>
+
+  );
+}
+
+const Folder = () => {
+  return (
+    <div>
+      <h3>Yo</h3>
+    </div>
+  );
+}
+
+export default App;
