f29bbdab92b686dd0e2c29acc38a4b0cab5b7690
Hao Chiang <haomingchiang@gmail.com>
2021-03-24 17:50:10 -0700

    finished File and Folder components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import {useState} from 'react'
 
 function App() {
   return (
     <div>
       <Folder name='Desktop'>
+        <Folder name='music'>
+          <File name='all_star.mp4' />
+          <File name='express_file.mp4' />
+        </Folder>
         <File name='cat.png' />
         <File name='dog.jpeg' />
       </Folder>
@@ -12,20 +17,42 @@ function App() {
 }
 
 const Folder = (props) => {
+  
+  const [isOpen, setIsOpen] = useState(true);
+  const {name, children} = props;
+  const direction = isOpen ? 'down' : 'right';
+
+  const handleClick = () => {
+    setIsOpen(!isOpen) ;
+  }
+  
   return (
     <div>
-      {props.name}
+      <span onClick={handleClick}>
+        <i className="green folder icon"></i>
+        <i className={`caret ${direction} icon`}></i>
+      </span>
+      {name}
       <div style={{marginLeft: '17px'}}>
-        {props.children}
+        {isOpen ? children : null}
       </div>
     </div>
   );
 }
 
 const File = (props) => {
+  const {name} = props;
+  const fileExtension = name.split('.')[1];
+  const fileIcons = {
+    mp4: 'music',
+    png: 'file image',
+    jpeg: 'file image outline'
+  };
+
   return (
     <div>
-      {props.name}
+      <i className={`${fileIcons[fileExtension]} icon`}></i>
+      {name}
     </div>
   );
 }
